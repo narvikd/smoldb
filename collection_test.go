ec156d313a03b6a146f8e494267e4f405e3dc7fa
narvikd <84069271+narvikd@users.noreply.github.com>
2022-06-14 18:29:10 +0200

    Move numBetween to test
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- collection_test.go
+++ collection_test.go
@@ -1,8 +1,10 @@
 package smoldb
 
 import (
+	cryptoRand "crypto/rand"
 	"github.com/stretchr/testify/require"
 	"log"
+	"math/big"
 	"os"
 	"strconv"
 	"testing"
@@ -145,4 +147,16 @@ func TestLenAllRecordsAndList(t *testing.T) {
 	}
 	require.Equal(t, 100, len(DB.GetAllRecords()))
 	require.Equal(t, 100, len(DB.GetAllRecordsKeys()))
+}
+
+// numBetween gives a random int64 based on min and max -> https://stackoverflow.com/a/26153749
+func numBetween(min int64, max int64) (int64, error) {
+	newMax := big.NewInt(max - min + 1)
+	// get big.Int between 0 and newMax
+	randNum, err := cryptoRand.Int(cryptoRand.Reader, newMax)
+	if err != nil {
+		return 0, errWrap(err, "error converting int to bigNum on numBetween")
+	}
+	// add min to randNum to return a number in the range of the input
+	return randNum.Int64() + min, nil
 }
